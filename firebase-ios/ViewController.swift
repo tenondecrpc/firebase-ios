77693ae353dc3093bb71806b27b664aa4461898c
Cristian Paniagua <cristianramonpaniagua@gmail.com>
2020-06-13 12:26:55 -0400

    Initial Commit

---
+++ firebase-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebase-ios
+//
+//  Created by Cristian Paniagua on 06/13/20.
+//  Copyright © 2020 Cristian Paniagua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
